828c9bf2fc9cc66f57cb0b0a9f951d679cf24e80
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-25 15:45:17 -0300

    feat: create user store methods

--- src/stores/auth-store.js
+++ src/stores/auth-store.js
@@ -1,20 +1,28 @@
 import { defineStore } from 'pinia';
 
+const loggedUser = window.sessionStorage.getItem('loggedUser');
 const sessionToken = window.sessionStorage.getItem('token');
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
+    user: loggedUser ?? null,
     token: sessionToken ?? null,
   }),
 
   getters: {
     getToken: (state) => state.token,
+    getUser: (state) => state.user,
   },
 
   actions: {
     setToken(token) {
       window.sessionStorage.setItem('token', token);
       this.token = token;
+    },
+
+    setUser(user) {
+      window.sessionStorage.setItem('loggedUser', user);
+      this.user = user;
     },
   },
 });

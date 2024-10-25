e2f6fd1f198f50c785f2d9881c1a1b754d7ecefe
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-25 18:42:51 -0300

    chore: add beforeEach on router file

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import { route } from 'quasar/wrappers';
 import {
   createRouter, createMemoryHistory, createWebHistory, createWebHashHistory,
 } from 'vue-router';
+import { storeToRefs } from 'pinia';
 import routes from './routes';
+import { useAuthStore } from '../stores/auth-store';
 
 /*
  * If not building with SSR mode, you can
@@ -26,6 +28,21 @@ export default route((/* { store, ssrContext } */) => {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
+
+  // eslint-disable-next-line consistent-return
+  Router.beforeEach((to) => {
+    const authStore = useAuthStore();
+    const { getUser } = storeToRefs(authStore);
+    const isLoggedIn = () => !!getUser.value;
+
+    if ((to.path === '/login' || to.path === '/') && isLoggedIn()) return { name: 'me' };
+
+    if (
+      !isLoggedIn()
+      && to.meta.requiresAuth
+      && !Object.keys(to.query).includes('fromEmail')
+    ) return { name: 'login' };
   });
 
   return Router;

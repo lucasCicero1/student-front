ae0aa459f1cd476a0c57418240b1c8763a84956d
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-25 23:22:24 -0300

    feat: create UseApi composable

---
+++ src/composables/UseApi.js
@@ -0,0 +1,17 @@
+import { api as http } from '../boot/axios';
+
+export default function useApi() {
+  const list = async (path) => http.get(path);
+  const create = async (path, payload) => http.post(path, payload);
+  const getById = async (path, id) => http.get(`${path}?${id}`);
+  const update = async (path, payload) => http.patch(path, payload);
+  const remove = async (path, payload) => http.delete(path, { data: payload });
+
+  return {
+    list,
+    create,
+    getById,
+    update,
+    remove,
+  };
+}

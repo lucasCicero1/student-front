a59d764f7652d43a3368ea07a41ffc9758b0a07a
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-29 14:29:24 -0300

    refactor: improves useApi methods

--- src/composables/UseApi.js
+++ src/composables/UseApi.js
@@ -1,11 +1,11 @@
 import { api as http } from '../boot/axios';
 
 export default function useApi() {
-  const list = async (path) => http.get(path);
+  const list = async ({ path }) => http.get(path);
   const create = async ({ path, payload }) => http.post(path, payload);
-  const getById = async (path, id) => http.get(`${path}?${id}`);
-  const update = async (path, payload) => http.patch(path, payload);
-  const remove = async (path, payload) => http.delete(path, { data: payload });
+  const getById = async ({ path, id }) => http.get(`${path}?${id}`);
+  const update = async ({ path, payload }) => http.patch(path, payload);
+  const remove = async ({ path, payload }) => http.delete(path, { data: payload });
 
   return {
     list,

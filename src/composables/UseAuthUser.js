f49799b4db8b9b6d388c844a7857e1be775b477c
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-29 17:08:00 -0300

    refactor: remover useless code

--- src/composables/UseAuthUser.js
+++ src/composables/UseAuthUser.js
@@ -1,50 +1,10 @@
-/* eslint-disable no-unused-vars */
-import { ref } from 'vue';
-import { api as http } from '../boot/axios';
-
-const user = ref({
-  loggedIn: false,
-  username: null,
-});
-
 export default function useAuthUser() {
-  const login = async ({ email, password }) => {
-    const data = { username: 'some-user', accessToken: 'Bearer: some-token' };
-    user.value.username = data.username;
-    user.value.loggedIn = true;
-    return { accessToken: data.accessToken, username: data.username };
-  };
-
-  const logout = async () => {
-    user.value.loggedIn = false;
-    user.value.username = null;
-  };
-
-  const isLoggedIn = () => !!user.value.loggedIn;
-
-  const register = async (payload) => {
-    // implementar lógica para enviar email de confirmação de cadastro
-    const { data } = await http.post('/signup', { payload });
-    return data;
-  };
-
-  // implementar lógica para atualização da senha
-  const update = async (data) => {};
-
-  // implementar lógica de enviar um email para o usuario resetar a senha
-  const sendPasswordRestEmail = async (email) => {};
-
-  // implementar lógica de resetar a senha
-  const resetPassword = async (accessToken, newPassword) => {};
-
+  const login = async () => ({ accessToken: 'Bearer: some-token', username: 'Lucas Silva' });
+  const logout = async () => {};
+  const register = async () => {};
   return {
-    user,
     login,
     logout,
-    isLoggedIn,
     register,
-    update,
-    sendPasswordRestEmail,
-    resetPassword,
   };
 }
--- src/pages/Me.vue
+++ src/pages/Me.vue
@@ -1,25 +1,14 @@
 <template>
-  <q-page class="flex flex-center">
-    <div v-if="user">
-      <!-- <p>Hello {{ user }}</p> -->
-      <p>Hello {{ user.username }}</p>
-    </div>
+  <q-page>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import useAuthUser from 'src/composables/UseAuthUser';
 
 export default defineComponent({
   name: 'MePage',
 
-  setup() {
-    const { user } = useAuthUser();
-
-    return {
-      user,
-    };
-  },
+  setup() {},
 });
 </script>

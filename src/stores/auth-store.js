0baec2dbf558b93cd14d1b84fe9235195280bbd4
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-29 17:09:33 -0300

    refactor: replace sessionStorate to localStorage on auth-store

--- src/stores/auth-store.js
+++ src/stores/auth-store.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 
-const loggedUser = window.sessionStorage.getItem('loggedUser');
-const sessionToken = window.sessionStorage.getItem('token');
+const loggedUser = window.localStorage.getItem('loggedUser');
+const sessionToken = window.localStorage.getItem('token');
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
@@ -16,18 +16,18 @@ export const useAuthStore = defineStore('auth', {
 
   actions: {
     setToken(token) {
-      window.sessionStorage.setItem('token', token);
+      window.localStorage.setItem('token', token);
       this.token = token;
     },
 
     setUser(user) {
-      window.sessionStorage.setItem('loggedUser', user);
+      window.localStorage.setItem('loggedUser', user);
       this.user = user;
     },
 
     logout() {
-      window.sessionStorage.removeItem('token');
-      window.sessionStorage.removeItem('loggedUser');
+      window.localStorage.removeItem('token');
+      window.localStorage.removeItem('loggedUser');
       this.user = null;
       this.token = null;
     },
